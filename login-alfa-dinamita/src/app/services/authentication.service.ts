dc3fd982cc8bfcdb534a6e9ba7a47828d2ce9112
SamantaGomez <samygomezjacome@gmail.com>
2021-01-06 21:18:35 -0500

    apk generada

---
+++ login-alfa-dinamita/src/app/services/authentication.service.ts
@@ -0,0 +1,60 @@
+// authentication.service.ts
+import { Injectable } from "@angular/core";
+import { AngularFireAuth } from '@angular/fire/auth';
+import auth from 'firebase/app';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticateService {
+
+  constructor(
+    private afAuth: AngularFireAuth
+  ) { }
+
+  loginGoogle(){
+    try{
+      return this.afAuth.signInWithPopup(new auth.auth.GoogleAuthProvider());
+    }catch(error){
+      console.log(error);
+    }
+  }
+  registerUser(value) {
+    return new Promise<any>((resolve, reject) => {
+
+      this.afAuth.createUserWithEmailAndPassword(value.email, value.password)
+        .then(
+          res => resolve(res),
+          err => reject(err))
+    })
+
+  }
+
+  loginUser(value) {
+    return new Promise<any>((resolve, reject) => {
+      this.afAuth.signInWithEmailAndPassword(value.email, value.password)
+        .then(
+          res => resolve(res),
+          err => reject(err))
+    })
+  }
+
+  logoutUser() {
+    return new Promise((resolve, reject) => {
+      if (this.afAuth.currentUser) {
+        this.afAuth.signOut()
+          .then(() => {
+            console.log("LOG Out");
+            //resolve();
+          }).catch((error) => {
+            reject();
+          });
+      }
+    })
+  }
+
+  userDetails() {
+    return this.afAuth.user
+  }
+}
---
+++ login-alfa-dinamita/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyAo_ecsDbvwReZIkGSK78jiiQFxqPRE008",
+    authDomain: "login-alfa-dinamita.firebaseapp.com",
+    projectId: "login-alfa-dinamita",
+    storageBucket: "login-alfa-dinamita.appspot.com",
+    messagingSenderId: "190514734376",
+    appId: "1:190514734376:web:146485790752680ef45c1d",
+    measurementId: "G-04SCW5WK6P"
+  }};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
